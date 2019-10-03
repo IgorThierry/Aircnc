9632ea93ecf5195e2dd741acbadc79f672358c4c
Igor Thierry <igorthierry15@gmail.com>
2019-10-02 23:16:48 -0300

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,23 @@ import React from 'react';
 
 import './App.css';
 
+import logo from './assets/logo.svg';
+
+import Routes from './routes';
+
 function App() {
+
   return (
-    <h1>Teste</h1>
+    <div className="container">
+      <img src={logo} alt="Aircnc" />
+
+      <div className="content">
+
+        <Routes />
+
+      </div>
+
+    </div>
   );
 }
 
