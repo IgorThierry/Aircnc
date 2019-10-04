750f0e69432b53d9fba57beb5b074f3b864abb31
Igor Thierry <igorthierry15@gmail.com>
2019-10-04 15:07:02 -0300

    finalizado frontend web

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -12,6 +13,7 @@ mongoose.connect('mongodb+srv://omnistack:pspdmc123@omnistack-9pkeq.mongodb.net/
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname,'..','uploads')));
 app.use(routes);
 
 app.listen(3333);
